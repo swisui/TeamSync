10746027c786c52a767c4b08aba69671631f8670
Nitin Mishra <nitinkrmr@gmail.com>
2023-04-11 19:29:11 +0530

    dashboard changes WIP

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { Project } from '../../models/project.model';
+import { Comment } from '../../models/comment.model';
 
 @Component({
     selector: 'app-dashboard',
@@ -6,8 +9,12 @@ import { Component } from '@angular/core';
     styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-    urgentTask: Array<{ id: number; name: string; completed: boolean; due: Date }>;
     public now: Date;
+    public urgentTask: Array<Task>;
+
+    public projectDirectory: Array<Project>;
+    public newComments: Array<Comment>;
+    public teamDirectory: Array<{ id: number; name: string; completed: boolean; due: Date }>;
 
     constructor() {
         this.now = new Date();
@@ -18,5 +25,40 @@ export class DashboardComponent {
             { id: 3, name: 'Deploy project to dev & staging', completed: false, due: new Date() },
             { id: 3, name: 'Prepare documentation', completed: false, due: new Date() },
         ];
+        this.projectDirectory = [
+            {
+                name: 'Market Research 2024',
+                user: [
+                    { name: 'David', profilePicUrl: '' },
+                    { name: 'Paul', profilePicUrl: '' },
+                ],
+            },
+            { name: 'New Proposals', user: [{ name: 'David', profilePicUrl: '' }] },
+            { name: 'Brand sprints', user: [{ name: 'David', profilePicUrl: '' }] },
+            {
+                name: 'Customer experience Q3',
+                user: [
+                    { name: 'David', profilePicUrl: '' },
+                    { name: 'Paul', profilePicUrl: '' },
+                    { name: 'John', profilePicUrl: '' },
+                ],
+            },
+            { name: 'Report signing', user: [{ name: 'David', profilePicUrl: '' }] },
+        ];
+        this.newComments = [
+            {
+                user: 'John',
+                project: 'Market Research 2024',
+                message: 'Find my keynote attached in the documents',
+                profileImageUrl: '',
+            },
+            {
+                user: 'Dana R.',
+                project: 'Market Research 2024',
+                message: "I've added some new data. Let's connect after daily stand up.",
+                profileImageUrl: '',
+            },
+        ];
+        this.teamDirectory = [];
     }
 }
---
+++ src/app/models/comment.model.ts
@@ -0,0 +1,6 @@
+export interface Comment {
+    user: string;
+    project: string;
+    message: string;
+    profileImageUrl: string;
+}
