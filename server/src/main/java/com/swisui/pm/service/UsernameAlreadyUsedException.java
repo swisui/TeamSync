6b03d9e67e2a8c563d851c92e03270219d3f7493
Nitin Mishra <nitinkrmr@gmail.com>
2023-04-13 15:39:54 +0530

    project cleanup

---
+++ server/src/main/java/com/swisui/pm/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,10 @@
+package com.swisui.pm.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+}
---
+++ server/src/main/java/com/swisui/pm/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.swisui.pm.service.dto;
