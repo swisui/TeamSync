58a5aabd377904e286c4ecd96ff3bf74e3b79f05
Nitin Mishra <nitinkrmr@gmail.com>
2023-04-13 22:49:29 +0530

    Overview change WIP

--- client/src/app/layout/overview/overview.module.ts
+++ client/src/app/layout/overview/overview.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { OverviewRoutingModule } from './overview-routing.module';
 import { OverviewComponent } from './overview.component';
+import {AvatarModule} from "primeng/avatar";
+import {InputTextModule} from "primeng/inputtext";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {ChartModule} from "primeng/chart";
+import {DropdownModule} from "primeng/dropdown";
 
 @NgModule({
     declarations: [OverviewComponent],
-    imports: [CommonModule, OverviewRoutingModule],
+    imports: [CommonModule, OverviewRoutingModule, AvatarModule, InputTextModule, CardModule, ButtonModule, RippleModule, ChartModule, DropdownModule],
 })
 export class OverviewModule {}
---
+++ client/src/app/shared/utils/chartjs.utils.ts
@@ -0,0 +1,36 @@
+const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+
+const COLORS = ['#4dc9f6', '#f67019', '#f53794', '#537bc4', '#acc236', '#166a8f', '#00a950', '#58595b', '#8549ba'];
+
+export const CHART_COLORS = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)',
+};
+
+const NAMED_COLORS = [
+    CHART_COLORS.red,
+    CHART_COLORS.orange,
+    CHART_COLORS.yellow,
+    CHART_COLORS.green,
+    CHART_COLORS.blue,
+    CHART_COLORS.purple,
+    CHART_COLORS.grey,
+];
