a4e5aae7e9e9da9043a7a3514e804603a284eb18
Nitin Mishra <nitinkrmr@gmail.com>
2023-04-20 00:21:03 +0530

    UI Changes

--- client/src/app/layout/components/form-component/form-component.module.ts
+++ client/src/app/layout/components/form-component/form-component.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
+import { InputTextModule } from "primeng/inputtext";
 import { FormComponentRoutingModule } from './form-component-routing.module';
 import { FormComponentComponent } from './form-component.component';
 
 @NgModule({
     declarations: [FormComponentComponent],
-    imports: [CommonModule, FormComponentRoutingModule, CardModule]
+    imports: [CommonModule, FormComponentRoutingModule, CardModule, InputTextModule, ButtonModule]
 })
 export class FormComponentModule {}
