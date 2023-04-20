cc1981cbc8f6ad041885ca1f0b3835af870c28ba
Nitin Mishra <nitinkrmr@gmail.com>
2023-04-20 11:46:50 +0530

    ui changes

--- client/src/app/layout/components/form-component/form-component.component.ts
+++ client/src/app/layout/components/form-component/form-component.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'form-component',
     templateUrl: './form-component.component.html',
     styleUrls: ['./form-component.component.scss']
 })
-export class FormComponentComponent {}
+export class FormComponentComponent {
+    public form: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+        this.form = this.formBuilder.group({
+            firstname: [null, Validators.required],
+            lastname: [null, Validators.required],
+            address: [null, Validators.required],
+            city: [null, Validators.required],
+            state: [null, Validators.required],
+            zip: [null, Validators.required]
+        });
+    }
+
+    onSubmit() {
+        console.log(this.form.value);
+    }
+}
